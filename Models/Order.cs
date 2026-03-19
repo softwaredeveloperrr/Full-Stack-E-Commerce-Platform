ed787937ed1f3171374c17412d6e2b090e726fe6
softwaredeveloperrr <jitenderpalsingh16@gmail.com>
2026-03-19 00:07:16 -0700

    Create Order.cs

---
+++ Models/Order.cs
@@ -0,0 +1,7 @@
+public class Order
+{
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public DateTime OrderDate { get; set; } = DateTime.UtcNow;
+    public decimal Total { get; set; }
+}
